94afa8d6b7f9373de766cbac2d4524c9750b6ade
unknown <kilq2reaper@hotmail.com>
2019-01-15 22:08:28 +0700

    modified port variable

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 
 const publicPath = path.join(__dirname, '../public');
-const port = path.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = new express();
 
 app.use(express.static(publicPath));
