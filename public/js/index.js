d6f7244a3188453f77115d50d620cb9d62364a42
unknown <kilq2reaper@hotmail.com>
2019-01-21 16:08:19 +0700

    huh

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,12 @@ socket.on("connect", function() {
     socket.on("roomList", function(rooms){
         console.log(rooms);
         if(rooms.length  > 0){
-            jQuery('#dropdown-label').html('<label>Or click below to join</label>');
+            jQuery('#dropdown-label').html('<label>OR join an existing room</label>');
             let ddl = jQuery('<select></select');
             ddl.attr('id','joining-room')
-            let blank =jQuery('<option></option>');
-            blank.attr('disabled','disabled').attr('text', '').attr('selected','selected'); 
-            ddl.append(blank);
+     
+            
+            
             
             rooms.forEach(room =>{
                 ddl.append(jQuery('<option></option>').val(room).text(room));
